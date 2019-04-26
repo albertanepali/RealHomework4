d37f08f3dd06d65a728567ce75979eeb17fd2e33
albertanepali <albertanepali@gmail.com>
2019-04-25 21:57:49 -0600

    server

--- server.js
+++ server.js
@@ -1,42 +1,46 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var passport = require('passport');
-var authJwtController = require('./auth_jwt');
-var User = require('./Users');
-var jwt = require('jsonwebtoken');
-var Movie = require('./Movies');
-var Comment = require('./Comments');
-mongodb = require('mongodb');
-ObjectId = mongodb.ObjectId;
-var app = express();
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+let passport = require('passport');
+let authJwtController = require('./auth_jwt');
+let User = require('./Users');
+let jwt = require('jsonwebtoken');
+let Movie = require('./Movies');
+let Comment = require('./Comments');
 module.exports = app; // for testing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(passport.initialize());
+
 var router = express.Router();
+
 router.route('/postjwt')
     .post(authJwtController.isAuthenticated, function (req, res) {
-        console.log(req.body);
-        res = res.status(200);
-        if (req.get('Content-Type')) {
-            console.log("Content-Type: " + req.get('Content-Type'));
-            res = res.type(req.get('Content-Type'));
+            console.log(req.body);
+            res = res.status(200);
+            if (req.get('Content-Type')) {
+                console.log("Content-Type: " + req.get('Content-Type'));
+                res = res.type(req.get('Content-Type'));
+            }
+            res.send(req.body);
         }
-        res.send(req.body);
-    });
+    );
 
-router.route('/users/:userId')
+
+app.route('/users/:userId')
     .get(authJwtController.isAuthenticated, function (req, res) {
         var id = req.params.userId;
         User.findById(id, function(err, user) {
             if (err) res.send(err);
             var userJson = JSON.stringify(user);
             // return that user
-            res.json(userJson );
+            res.json(userJson);
         });
     });
 
-router.route('/users')
+
+app.route('/users')
     .get(authJwtController.isAuthenticated, function (req, res) {
         User.find(function (err, users) {
             if (err) res.send(err);
@@ -45,7 +49,8 @@ router.route('/users')
         });
     });
 
-router.post('/signup', function(req, res) {
+
+app.post('/signup', function(req, res) {
     if (!req.body.username || !req.body.password) {
         res.json({success: false, msg: 'Please pass username and password.'});
     }
@@ -63,63 +68,14 @@ router.post('/signup', function(req, res) {
                 else
                     return res.send(err);
             }
+
             res.json({ success: true, message: 'User created!' });
         });
     }
 });
-router.route('/Movies/:moviesid')
-    .get(authJwtController.isAuthenticated, function (req, res) {
-        var id = req.params.moviesid;
-        Movie.findById(id, function (err, movie) {
-            if (err) res.send(err);
-            var movieJson = JSON.stringify(movie);
-            res.json(movieJson);
-        })
-    });
 
-router.route('/Movies')
-    .get(authJwtController.isAuthenticated, function (req, res) {
-        Movie.findOne({title : req.body.title}, function (err, movies) {
-            if (err) res.send(err);
-            res.json(movies);
-        })
-    });
 
-router.route('/MoviesAll')
-    .get(authJwtController.isAuthenticated, function (req, res) {
-        Movie.find(function (err, movies) {
-            if(err) res.send(err);
-            res.json(movies);
-        })
-    });
-router.route('/Movies/:id')
-    .put(authJwtController.isAuthenticated, function (req, res) {
-        var conditions = {_id: req.params.id};
-        Movie.updateOne(conditions, req.body)
-            .then(mov => {
-                if (!mov) {
-                    return res.status(404).end();
-                }
-                return res.status(200).json({msg: "Movie updated"})
-            })
-            .catch(err => next(err))
-    });
-router.route('/Movies')
-    .delete(authJwtController.isAuthenticated, function (req, res){
-        Movie.findOneAndDelete({title: req.body.title}, function (err, movie) {
-            if (err)
-            {
-                res.status(400).json({msg: err})
-            }
-            else if(movie == null)
-            {
-                res.json({msg : "Movie not found"})
-            }
-            else
-                res.json({msg :"The movie was deleted"})
-        })
-    });
-router.route('/Movies')
+app.route('/Movies')
     .post(authJwtController.isAuthenticated, function (req, res) {
         console.log(req.body);
         var movies = new Movie();
@@ -137,7 +93,38 @@ router.route('/Movies')
             res.json({success: true, message: 'Movie saved!'})
         });
     });
-router.route('/Comments')
+
+
+app.route('/movies/:moviesid')
+    .get(authJwtController.isAuthenticated, function (req, res) {
+        var id = req.params.moviesid;
+        Movie.findById(id, function (err, movie) {
+            if (err) res.send(err);
+            var movieJson = JSON.stringify(movie);
+            res.json(movieJson);
+        })
+    });
+
+
+app.route('/Moviesa')
+    .get(authJwtController.isAuthenticated, function (req, res) {
+        Movie.findOne({title : req.body.title}, function (err, movies) {
+            if (err) res.send(err);
+            res.json(movies);
+        })
+    });
+
+
+app.route('/comments')
+    .get(authJwtController.isAuthenticated, function (req, res) {
+        Comment.find(function (err, comment) {
+            if(err) res.send(err);
+            res.json(comment);
+        })
+    });
+
+
+app.route('/Comments')
     .post(authJwtController.isAuthenticated, function (req, res) {
         const usertoken = req.headers.authorization;
         const token = usertoken.split(' ');
@@ -167,70 +154,54 @@ router.route('/Comments')
         });
     });
 
-router.route('/MoviesandComment')
+app.route('/MoviesAll')
     .get(authJwtController.isAuthenticated, function (req, res) {
-        var data = req.body;
-        Movie.aggregate([
-            {
-                "$match": {"title": data.title}
-            },
-            {
-                $lookup:
-                    {
-                        from: 'comments',
-                        localField: 'title',
-                        foreignField: 'title',
-                        as: 'reviews'
-                    },
-            },
-        ]).exec((err, review) => {
-            if (err) {
-                res.status('500').send(err);
-            } else {
-                res.json(review)
-            }
-        });
+        Movie.find(function (err, movies) {
+            if(err) res.send(err);
+            res.json(movies);
+        })
     });
 
-router.route('/movies')
-    .get(authJwtController.isAuthenticated, function(req, res){
-        console.log("Getting movies and reviews from database");
-        if(req.query.reviews == 'true'){
-            console.log("Getting movies with review");
-            Movie.aggregate([
-                {
-                    $lookup: {
-                        from: "reviews",
-                        localField: "title",
-                        foreignField: "movietitle",
-                        as : 'reviews'
-                    }
+
+app.route('/Movies/:id')
+    .put(authJwtController.isAuthenticated, function (req, res) {
+        var conditions = {_id: req.params.id};
+        Movie.updateOne(conditions, req.body)
+            .then(mov => {
+                if (!mov) {
+                    return res.status(404).end();
                 }
+                return res.status(200).json({msg: "Movie updated"})
+            })
+            .catch(err => next(err))
+    });
 
-            ],function (err, result){
-                if(err){
-                    res.send(err);
 
-                }
-                else res.send(result);
+app.route('/Movies')
+    .delete(authJwtController.isAuthenticated, function (req, res){
+        Movie.findOneAndDelete({title: req.body.title}, function (err, movie) {
+            if (err)
+            {
+                res.status(400).json({msg: err})
+            }
+            else if(movie == null)
+            {
+                res.json({msg : "Movie not found"})
+            }
+            else
+                res.json({msg :"The movie was deleted"})
+        })
+    });
 
-            });
-        }
-        else {
-            console.log("Getting movies without reviews")
-            Movie.find(function (err, movies){
-                if(err) res.send(err);
-                res.json(movies);
-            });
-        }
-    })
-router.post('/signin', function(req, res) {
+app.post('/signin', function(req, res) {
     var userNew = new User();
     userNew.name = req.body.name;
     userNew.username = req.body.username;
     userNew.password = req.body.password;
+
     User.findOne({username: userNew.username}).select('name username password').exec(function (err, user) {
         if (err) res.send(err);
+
         user.comparePassword(userNew.password, function (isMatch) {
             if (isMatch) {
                 var userToken = {id: user._id, username: user.username};
@@ -241,9 +212,49 @@ router.post('/signin', function(req, res) {
             }
         });
     });
-    router.all('*', function (res, req) {
+    app.all('*', function (res, req) {
         req.json({error: 'Does not support the HTTP method'});
     });
 });
+
+
+app.route('/movies')
+    .get(authJwtController.isAuthenticated, function (req, res) {
+        let data = req.body;
+        if(req.query.reviews === 'true')
+        {
+            Movie.aggregate([
+                {
+                    "$match": {"title": data.title}
+                },
+                {
+                    $lookup:
+                        {
+                            from: 'comments',
+                            localField: 'title',
+                            foreignField: 'title',
+                            as: 'reviews'
+                        },
+                },
+            ]).exec((err, review) => {
+                if (err) {
+                    res.status('500').send(err);
+                } else {
+                    res.json(review)
+                }
+            });
+        }else {
+            Movie.find(function (err, movies) {
+                if ( err)
+                {
+                    res.send(err);
+                }
+                else {
+                    res.json(movies)
+                }
+            })
+        }
+    });
+
 app.use('/', router);
-app.listen(process.env.PORT || 1000);
+app.listen(process.env.PORT || 5000);
